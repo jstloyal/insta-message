d8da7b5b4d81d13f3fde8008157267803aa62632
jstloyal <jstloyalty@gmail.com>
2020-07-16 21:27:25 +0100

    build message and user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create( username: "Akanmu", password: "password")
+User.create( username: "Bimpe", password: "password")
+User.create( username: "Tayo", password: "password")
+User.create( username: "Bobo", password: "password")
+User.create( username: "Wale", password: "password")
+
+Message.create( body: "how are you doing?", user_id: 1)
+Message.create( body: "I'm fine", user_id: 4)
+Message.create( body: "how is your baby?", user_id: 1)
+Message.create( body: "he is also fine", user_id: 4)
+Message.create( body: "you're back?", user_id: 5)
