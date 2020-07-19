40766928aed179a231572142ac9d9877d3a80926
jstloyal <jstloyalty@gmail.com>
2020-07-19 03:46:33 +0100

    Enable flash message display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = 'You have succesfully logged out!'
     redirect_to login_path
   end
 
