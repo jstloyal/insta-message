000f9c0643a63c0e210d707b47182280fee49199
jstloyal <jstloyalty@gmail.com>
2020-07-19 03:27:05 +0100

    add authentication system - create and destroy sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
                       length: { minimum: 3, maximum: 20},
                       uniqueness: { case_sensitive: false }
   has_many :messages
-  # has_secure_password
+  has_secure_password
 end
